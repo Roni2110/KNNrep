2a022051d6cef1eee637d92d3226b54a0a4c0c9a
Danashl <danashlasky@gmail.com>
2022-12-08 10:34:51 +0200

    changes in main to one vector

--- main.cpp
+++ main.cpp
@@ -11,40 +11,39 @@
 
 using namespace std;
 
-/**
- * this function checks that the two vectors are at the same size.
- * also, this function checks that that the vectors are not empty - that their size is not 0.
- * if they have no values or they are not at the same size - it prints an error to the user.
- * @param vec1 - the first vector.
- * @param vec2 - the second vector.
- */
-void checkingVec(vector<double>vec1, vector<double> vec2) {
-    if(vec1.size() > vec2.size() || vec2.size() > vec1.size()) {
-        cout << "The vectors are in different size!" << endl;
-        exit(0);
-    } else if (vec1.size() == 0) {
-        cout << "No parameters added!" << endl;
-        exit(0);
-    } else {
-        DistanceClass::getEucDis(vec1, vec2);
-        DistanceClass::getManDis(vec1, vec2);
-        DistanceClass::getChebDis(vec1, vec2);
-        DistanceClass::getCanDis(vec1, vec2);
-        DistanceClass::getMinkDis(vec1, vec2);
-    }
-}
+// /**
+//* this function checks that the two vectors are at the same size.
+//* also, this function checks that that the vectors are not empty - that their size is not 0.
+//* if they have no values or they are not at the same size - it prints an error to the user.
+// * @param vec1 - the first vector.
+//* @param vec2 - the second vector.
+//*/
+
+//void checkingVec(vector<double>vec1, vector<double> vec2) {
+  //  if(vec1.size() > vec2.size() || vec2.size() > vec1.size()) {
+  //    cout << "The vectors are in different size!" << endl;
+    //  exit(0);
+    //} else if (vec1.size() == 0) {
+    //    cout << "No parameters added!" << endl;
+    //    exit(0);
+    //} else {
+    //    DistanceClass::getEucDis(vec1, vec2);
+    //    DistanceClass::getManDis(vec1, vec2);
+    //    DistanceClass::getChebDis(vec1, vec2);
+    //    DistanceClass::getCanDis(vec1, vec2);
+    //    DistanceClass::getMinkDis(vec1, vec2);
+//    }
+//}
 
 /**
  * this function checks that all the values in the vectors are numbers. if not - it prints an error to the user.
  * @param str1 - the values of the first vector.
  * @param str2 - the values of the second vector.
  */
-void checkingInput(string str1, string str2) {
+void checkingInput(string str1) {
     vector<double> v1;
-    vector<double> v2;
     double num;
     stringstream stringstream1(str1);
-    stringstream stringstream2(str2);
     while(stringstream1 >> num) {
         v1.push_back(num);
     }
@@ -52,14 +51,10 @@ void checkingInput(string str1, string str2) {
         cout << "Input not valid!" << endl;
         exit(0);
     }
-    while (stringstream2 >> num) {
-        v2.push_back(num);
-    }
-    if(!stringstream2.eof()) {
-        cout << "Input not valid!" << endl;
+    if (v1.size() == 0) {
+        cout << "No parameters added!" << endl;
         exit(0);
     }
-    checkingVec(v1,v2);
 }
 
 /**
@@ -82,26 +77,15 @@ void checkingArgv(char *argv[]) {
  */
 int main (int argc, char *argv[]) {
     checkingArgv(argv);
-/*    cout << stoi(argv[1]) << endl;
-    cout << argv[2] << endl;
-    cout << argv[3] << endl;*/
     int k = stoi(argv[1]);
     string file = argv[2];
     string dis = argv[3];
-/*    cout << k << endl;
-    cout << file << endl;
-    cout << dis << endl;*/
     string input1;
-    string input2;
     getline(cin,input1);
-    getline(cin, input2);
     if((double)input1.size() >= log(DBL_MAX) - 1) {
-        input1 = input1.substr(0, sizeof(double ));
+        input1 = input1.substr(0, sizeof(double));
     }
-    if((double)input2.size() >= log(DBL_MAX) - 1) {
-        input2 = input2.substr(0, sizeof(double ));
-    }
-    checkingInput(input1,input2);
+    checkingInput(input1);
     Knn(k,file,dis);
     return 0;
 }
